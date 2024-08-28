f8d3a8a127f6288fec275336c3b55381c8e68c7d
cheungah <hoyam@graduate.hku.hk>
2024-08-27 23:22:24 -0700

    First commit

---
+++ target/classes/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "089b53e9-a214-4235-ba12-48b979c3c808";
+}
---
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -0,0 +1,5 @@
+package org.kie.kogito.app;
+@io.quarkus.runtime.Startup()
+public class HotReloadSupportClass {
+private static final String ID = "d93eec4d-3c19-4c28-bca0-6ee95899901d";
+}
